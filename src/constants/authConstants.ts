b210cc52ec2e0c367b1d1dd54ef8f062efd6eb58
Yamac Yurtsever <z5549363@ad.unsw.edu.au>
2024-11-23 18:51:00 +1100

    Added authRegister constants

---
+++ src/constants/authConstants.ts
@@ -0,0 +1,6 @@
+export const NAME_REGEX = /^[A-Za-z0-9 ]*$/;                     // Only letters, number, spaces
+export const NAME_MIN_LENGTH = 2;
+export const NAME_MAX_LENGTH = 20;
+export const PASSWORD_REGEXES = [/[a-zA-Z]/, /\d/];              // At least one letter, number
+export const PASSWORD_MIN_LENGTH = 8;
+export const SESSION_EXPIRATION_TIME = 30 * 24 * 60 * 60 * 1000; // 30 days
