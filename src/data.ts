7a27fd13e9f60173617292a1509a37cea7eb9cbf
Yamac Yurtsever <z5549363@ad.unsw.edu.au>
2024-11-23 11:02:57 +1100

    Created data.ts file

---
+++ src/data.ts
@@ -0,0 +1,21 @@
+import { Data } from './models/dataModel';
+
+const emptyData: Data = {
+  users: [],
+}
+
+let data = emptyData;
+
+const getData = () => {
+  return data;
+}
+
+const setData = (newData: Data) => {
+  data = newData;
+}
+
+const clearData = () => {
+  data = emptyData;
+}
+
+export { getData, setData, clearData };
