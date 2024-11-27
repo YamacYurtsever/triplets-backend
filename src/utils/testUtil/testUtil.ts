80e533c4b0283588311b2b21a3c3adc545a2bd3a
Yamac Yurtsever <z5549363@ad.unsw.edu.au>
2024-11-27 20:49:38 +1100

    Added empty object constant to testUtil.ts

--- src/utils/testUtil/testUtil.ts
+++ src/utils/testUtil/testUtil.ts
@@ -5,6 +5,7 @@ import { IncomingHttpHeaders } from 'http';
 const SERVER_URL = `${url}:${port}`;
 const TIMEOUT_MS = 5 * 1000;
 const ERROR = { error: expect.any(String) };
+const EMPTY = { };
 
 const parseBody = (res: Response) => {
   return JSON.parse(res.body.toString());
@@ -28,4 +29,4 @@ const requestHelper = (
   return request(method, SERVER_URL + endpoint, { headers, qs, json, timeout: TIMEOUT_MS });
 };
 
-export { ERROR, parseBody, requestHelper };
+export { ERROR, EMPTY, parseBody, requestHelper };
