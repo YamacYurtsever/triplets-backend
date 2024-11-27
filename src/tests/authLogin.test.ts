0e711cdfa8d8733c281e68f7ffe4151b561ac41b
Yamac Yurtsever <z5549363@ad.unsw.edu.au>
2024-11-27 20:53:17 +1100

    Updated authLogin.test.ts

--- src/tests/authLogin.test.ts
+++ src/tests/authLogin.test.ts
@@ -12,45 +12,37 @@ beforeEach(() => {
 });
 
 describe('POST /auth/register', () => {
-  test('returns error when there are no registered users', () => {
-    const res = requestAuthLogin(VALID_USER.email, VALID_USER.password);
+  beforeEach(() => {
+    requestAuthRegister(VALID_USER.name, VALID_USER.email, VALID_USER.password);
+  });
+
+  test('returns error for incorrect email', () => {
+    const res = requestAuthLogin('incorrect email', VALID_USER.password);
     expect(parseBody(res)).toStrictEqual(ERROR);
     expect(res.statusCode).toBe(400);
   });
 
-  describe('when there is a registered user', () => {
-    beforeEach(() => {
-      requestAuthRegister(VALID_USER.name, VALID_USER.email, VALID_USER.password);
-    });
+  test('returns error for incorrect password', () => {
+    const res = requestAuthLogin(VALID_USER.email, 'incorrect password');
+    expect(parseBody(res)).toStrictEqual(ERROR);
+    expect(res.statusCode).toBe(400);
+  });
 
-    test('returns error for incorrect email', () => {
-      const res = requestAuthLogin('incorrect email', VALID_USER.password);
-      expect(parseBody(res)).toStrictEqual(ERROR);
-      expect(res.statusCode).toBe(400);
-    });
+  test('has correct return type', () => {
+    const res = requestAuthLogin(VALID_USER.email, VALID_USER.password);
+    expect(parseBody(res)).toStrictEqual(TOKEN);
+    expect(res.statusCode).toBe(200);
+  });
 
-    test('returns error for incorrect password', () => {
-      const res = requestAuthLogin(VALID_USER.email, 'incorrect password');
-      expect(parseBody(res)).toStrictEqual(ERROR);
-      expect(res.statusCode).toBe(400);
-    });
+  test('generates unique tokens for the same user at each login', () => {
+    const res1 = requestAuthLogin(VALID_USER.email, VALID_USER.password);
+    expect(parseBody(res1)).toStrictEqual(TOKEN);
+    expect(res1.statusCode).toBe(200);
 
-    test('has correct return type', () => {
-      const res = requestAuthLogin(VALID_USER.email, VALID_USER.password);
-      expect(parseBody(res)).toStrictEqual(TOKEN);
-      expect(res.statusCode).toBe(200);
-    });
+    const res2 = requestAuthLogin(VALID_USER.email, VALID_USER.password);
+    expect(parseBody(res2)).toStrictEqual(TOKEN);
+    expect(res2.statusCode).toBe(200);
 
-    test('generates unique tokens for the same user at each login', () => {
-      const res1 = requestAuthLogin(VALID_USER.email, VALID_USER.password);
-      expect(parseBody(res1)).toStrictEqual(TOKEN);
-      expect(res1.statusCode).toBe(200);
-
-      const res2 = requestAuthLogin(VALID_USER.email, VALID_USER.password);
-      expect(parseBody(res2)).toStrictEqual(TOKEN);
-      expect(res2.statusCode).toBe(200);
-
-      expect(parseBody(res1)).not.toStrictEqual(parseBody(res2));
-    });
+    expect(parseBody(res1)).not.toStrictEqual(parseBody(res2));
   });
 });
